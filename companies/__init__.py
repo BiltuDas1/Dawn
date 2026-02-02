3ec62c6768a491b56d4f369d72bb19c28cfa531e
BiltuDas1 <billionto@gmail.com>
2026-02-02 13:15:58 +0530

    fix: Enabled companies

--- companies/__init__.py
+++ companies/__init__.py
@@ -3,9 +3,9 @@ from . import zoho, jpmorgan, oracle, ibm, honeywell, astreya
 
 
 WEBSITES: list[Website] = []
-# WEBSITES.append(Website(name="Zoho", helper=zoho.scrap_zoho))
-# WEBSITES.append(Website(name="JPMorgan", helper=jpmorgan.scrap_jpmorgan))
-# WEBSITES.append(Website(name="Oracle", helper=oracle.scrap_oracle))
-# WEBSITES.append(Website(name="IBM", helper=ibm.scrap_ibm))
-# WEBSITES.append(Website(name="Honeywell", helper=honeywell.scrap_honeywell))
+WEBSITES.append(Website(name="Zoho", helper=zoho.scrap_zoho))
+WEBSITES.append(Website(name="JPMorgan", helper=jpmorgan.scrap_jpmorgan))
+WEBSITES.append(Website(name="Oracle", helper=oracle.scrap_oracle))
+WEBSITES.append(Website(name="IBM", helper=ibm.scrap_ibm))
+WEBSITES.append(Website(name="Honeywell", helper=honeywell.scrap_honeywell))
 WEBSITES.append(Website(name="Astreya", helper=astreya.scrap_astreya))
